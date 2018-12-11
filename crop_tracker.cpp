8ec0369eed1381ae8cafb58481b670c2f4056de9
Yi Han <hanyiabc@live.com>
2018-12-11 02:30:10 -0500

    working version

--- crop_tracker.cpp
+++ crop_tracker.cpp
@@ -12,6 +12,7 @@ using cv::imread;
 using cv::imshow;
 using cv::Mat;
 using cv::Scalar;
+using cv::Vec2f;
 using cv::Vec3b;
 using cv::Vec4i;
 using cv::waitKey;
@@ -20,12 +21,14 @@ using std::cout;
 using std::string;
 using std::vector;
 
-void cropRowDetec(Mat m);
+#define DEBUGGING 0
+
+void cropRowDetec(Mat &m);
 void grayTransform(Mat &m);
 void skeletonize(Mat &m);
-void houghTransform(Mat &m);
-void filterSimLines(vector<Vec4i> &lines);
-double solveforB(const Vec4i & twoPts);
+vector<cv::Vec2f> houghTransform(Mat &m);
+void filterSimLines(vector<Vec2f> &lines);
+double solveforB(const Vec2f & twoPts);
 cv::Vec2d twoPoints2Polar(const cv::Vec4i &line);
 
 const int HOUGH_RHO = 2;				 //Distance resolution of the accumulator in pixels
@@ -33,38 +36,93 @@ const double HOUGH_ANGLE = CV_PI / 45.0; //Angle resolution of the accumulator i
 const int HOUGH_THRESH_MAX = 100;			  //Accumulator threshold parameter.Only those lines are returned that get enough votes
 const int HOUGH_THRESH_MIN = 10;
 const int HOUGH_THRESH_INCR = 1;
-const int NUMBER_OF_ROWS = 5;						 //how many crop rows to detect
+const int NUMBER_OF_ROWS = 3;						 //how many crop rows to detect
 const double THETA_SIM_THRESH = CV_PI / 30.0; //How similar two rows can be ----- 6 degrees
 const int RHO_SIM_THRESH = 8;						 //How similar two rows can be
-const double ANGLE_THRESH = CV_PI / 5.0;	//How steep angles the crop rows can be in radians -------- 30 degrees
+const double ANGLE_THRESH = CV_PI / 6.0;	//How steep angles the crop rows can be in radians -------- 30 degrees
 const string FILE_NAME = "crop_row_013.JPG";
 
 int main(int argc, char const *argv[])
 {
-	if(argc==2)
+	if(argc >= 2)
 	{
 		cout << "Read image from commandline\n";
-		Mat im = imread(argv[1]);
-		cropRowDetec(im);
+
+		for(size_t i = 1; i < argc; i++)
+		{
+			Mat im = imread(argv[i]);
+			if (im.empty())
+			{
+				cout << "Wrong File Name!\n";
+				cout << "File name is" << argv[i] << "\n";
+				return EXIT_FAILURE;
+			}
+			cropRowDetec(im);
+			string path = "output/" + string(argv[i]);
+			cout << "Writing to file:" << path << "\n";
+			cv::imwrite(path, im);
+		}
+
+		
 	}
 	if(argc == 1)
 	{
-		cout << "Read image from constant\n";
-		Mat im = imread(FILE_NAME);
-		cropRowDetec(im);
+		//cout << "Read image from constant\n";
+
+		cout << "Use webcam";
+		cv::VideoCapture cap(0); // open the default camera
+		if (!cap.isOpened())  // check if we succeeded
+			return EXIT_FAILURE;
+		while (true)
+		{
+			Mat im;
+			cap >> im;
+			cropRowDetec(im);
+			imshow("Crop Rows", im);
+			if (waitKey(30) >= 0) break;
+		}
+		/*Mat im = imread(FILE_NAME);
+		cropRowDetec(im);*/
 	}
 
 	cout << "Done\n";
-	return 0;
+	return EXIT_SUCCESS;
 }
 
-void cropRowDetec(Mat m)
+void cropRowDetec(Mat &m)
 {
+	Mat original; 
+	m.copyTo(original);
 	grayTransform(m);
+	if (DEBUGGING)
+	{
+		imshow("Gray", m);
+	}
 	skeletonize(m);
-	imshow("Skel", m);
-	waitKey(0);
-	houghTransform(m);
+	if (DEBUGGING)
+	{
+		imshow("Skele", m);
+	}
+		
+	auto lines = houghTransform(m);
+	for (size_t i = 0; i < lines.size(); i++)
+	{
+		float rho = lines[i][0], theta = lines[i][1];
+		cv::Point pt1, pt2;
+		double a = cos(theta), b = sin(theta);
+		double x0 = a * rho, y0 = b * rho;
+		pt1.x = cvRound(x0 + 1000 * (-b));
+		pt1.y = cvRound(y0 + 1000 * (a));
+		pt2.x = cvRound(x0 - 1000 * (-b));
+		pt2.y = cvRound(y0 - 1000 * (a));
+		cv::line(original, pt1, pt2, Scalar(0, 0, 255), 3, CV_AA);
+	}
+	m = original;
+	if (DEBUGGING)
+	{
+		imshow("Lines", m);
+		waitKey(0);
+	}
 }
 
 void grayTransform(Mat &m)
@@ -83,6 +141,7 @@ void grayTransform(Mat &m)
 
 		}
 	}
+
 	Mat temp;
 	cv::cvtColor(m, temp, cv::COLOR_BGR2GRAY);
 	m = temp;
@@ -118,42 +177,31 @@ void skeletonize(Mat &m)
 	m = skel;
 }
 
-void houghTransform(Mat &m)
+vector<cv::Vec2f> houghTransform(Mat &m)
 {
 
 	int hough_thresh = HOUGH_THRESH_MAX;
 	bool row_found = false;
 
-	vector<cv::Vec4i> lines;
-	vector<cv::Vec4i> filteredLines;
+	vector<cv::Vec2f> lines;
 	while(hough_thresh > HOUGH_THRESH_MIN && !row_found)
 	{
 		lines.clear();
-		filteredLines.clear();
-		cv::HoughLinesP(m, lines, HOUGH_RHO, HOUGH_ANGLE, hough_thresh, 2, 50);
+		cv::HoughLines(m, lines, HOUGH_RHO, HOUGH_ANGLE, hough_thresh);
 		
 		filterSimLines(lines);
 
 		hough_thresh -= HOUGH_THRESH_INCR;
-		if(filteredLines.size() >= NUMBER_OF_ROWS)
+		if(lines.size() >= NUMBER_OF_ROWS)
 		{
 			row_found = true;
 		}
 	}
 
-	Mat temp;
-	cv::cvtColor(m, temp, cv::COLOR_GRAY2BGR);
-
-
-	for (size_t i = 0; i < lines.size(); i++)
-	{
-		cv::line(temp, cv::Point(lines[i][0], lines[i][1]), cv::Point(lines[i][2], lines[i][3]), cv::Scalar(0, 0, 255), 3, 8);
-	}
-	imshow("Detected Lines", temp);
-	waitKey(0);
+	return lines;
 }
 
-double solveforB(const Vec4i &twoPts)
+double solveforB(const Vec2f &twoPts)
 {
 	if (twoPts[2] - twoPts[0] == 0)
 	{
@@ -163,14 +211,15 @@ double solveforB(const Vec4i &twoPts)
 	return twoPts[3] - twoPts[2] * m;
 }
 
-void filterSimLines(vector<Vec4i> &lines)
+void filterSimLines(vector<Vec2f> &lines)
 {
 	if (!lines.empty())
 	{
-		for (size_t i = lines.size() - 1; i > 0; i--)
+		for (int i = lines.size() - 1; i >= 0; i--)
 		{
-			auto rhoTheta = twoPoints2Polar(lines[i]);
-			if ( ( (ANGLE_THRESH < rhoTheta[1]) && (rhoTheta[1] < CV_PI - ANGLE_THRESH) || (-ANGLE_THRESH > rhoTheta[1]) && (rhoTheta[1] > ANGLE_THRESH - CV_PI) ) || abs(rhoTheta[1]) <= 0.0001)
+			if (  ( (ANGLE_THRESH <= lines[i][1]) && (lines[i][1] <= CV_PI - ANGLE_THRESH) )  || 
+				  /*( (-ANGLE_THRESH > lines[i][1]) && (lines[i][1] > ANGLE_THRESH - CV_PI) ) ||*/
+				lines[i][1] <= 0.0001)
 			{
 				lines.erase(lines.begin() + i);
 			}
@@ -181,17 +230,26 @@ void filterSimLines(vector<Vec4i> &lines)
 
 					if (j != i)
 					{
-						auto rhoTheta2 = twoPoints2Polar(lines[j]);
-						if (abs(rhoTheta[1] - rhoTheta2[1]) < THETA_SIM_THRESH)
+						if (abs(lines[i][1] - lines[j][1]) < THETA_SIM_THRESH)
 						{
-							lines.erase(lines.begin() + i);
-							break;
+							if (abs(lines[i][0] - lines[j][0]) < RHO_SIM_THRESH)
+							{
+								lines.erase(lines.begin() + i);
+								break;
+							}
+								
 						}
-						else if (abs(rhoTheta[0] - rhoTheta2[0]) < RHO_SIM_THRESH)
-						{
-							lines.erase(lines.begin() + i);
-							break;
-						}
+						
+						// if (abs(lines[i][1] - lines[j][1]) < THETA_SIM_THRESH)
+						// {
+						// 	lines.erase(lines.begin() + i);
+						// 	break;
+						// }
+						// else if (abs(lines[i][0] - lines[j][0]) < RHO_SIM_THRESH)
+						// {
+						// 	lines.erase(lines.begin() + i);
+						// 	break;
+						// }
 					}
 
 				}
